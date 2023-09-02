e92681a878408f77e06162e7b055a715b1278b57
Homa Wong <homawong@gmail.com>
2023-09-01 20:20:52 -0700

    test: improve pre-post test

--- logics/ansi/src/tokenizer.spec.ts
+++ logics/ansi/src/tokenizer.spec.ts
@@ -8,44 +8,60 @@ it('parse text', () => {
 
 it('parses query device code', () => {
 	expect(tokenizer(`${ESCAPE}[c`)).toEqual([{ type: 'query_device_code' }])
-	expect(tokenizer(`${ESCAPE}[cHello`)).toEqual([
-		{ type: 'query_device_code' },
+	expect(tokenizer(`Hello${ESCAPE}[cWorld`)).toEqual([
 		{
 			type: 'text',
 			text: 'Hello'
+		},
+		{ type: 'query_device_code' },
+		{
+			type: 'text',
+			text: 'World'
 		}
 	])
 })
 
 it('parses report device code', () => {
 	expect(tokenizer(`${ESCAPE}[00c`)).toEqual([{ type: 'report_device_code', code: '0' }])
-	expect(tokenizer(`${ESCAPE}[1230cHello`)).toEqual([
-		{ type: 'report_device_code', code: '123' },
+	expect(tokenizer(`Hello${ESCAPE}[1230cWorld`)).toEqual([
 		{
 			type: 'text',
 			text: 'Hello'
+		},
+		{ type: 'report_device_code', code: '123' },
+		{
+			type: 'text',
+			text: 'World'
 		}
 	])
 })
 
 it('parses query device status', () => {
 	expect(tokenizer(`${ESCAPE}[5n`)).toEqual([{ type: 'query_device_status' }])
-	expect(tokenizer(`${ESCAPE}[5nHello`)).toEqual([
-		{ type: 'query_device_status' },
+	expect(tokenizer(`Hello${ESCAPE}[5nWorld`)).toEqual([
 		{
 			type: 'text',
 			text: 'Hello'
+		},
+		{ type: 'query_device_status' },
+		{
+			type: 'text',
+			text: 'World'
 		}
 	])
 })
 
 it('parses report device status', () => {
 	expect(tokenizer(`${ESCAPE}[0n`)).toEqual([{ type: 'report_device_ok' }])
-	expect(tokenizer(`${ESCAPE}[0nHello`)).toEqual([
-		{ type: 'report_device_ok' },
+	expect(tokenizer(`Hello${ESCAPE}[0nWorld`)).toEqual([
 		{
 			type: 'text',
 			text: 'Hello'
+		},
+		{ type: 'report_device_ok' },
+		{
+			type: 'text',
+			text: 'World'
 		}
 	])
 })
